56a2e232ab2a711f5c0432e5c151d3683d52878e
Mullins182 <Mullins182@t-online.de>
2024-09-02 12:51:40 +0200

    debugging is in progress !

---
+++ MyLittleDesktopFella/FellaWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyLittleDesktopFella
+{
+    public partial class FellaWindow : Window
+    {
+        private Random rN = new();
+        public Canvas MainCanvas = new();
+        public Rectangle FellaRect = new();
+        public BitmapImage FellaImage = new();
+        public ImageBrush FellaImageBrush = new();
+        public DoubleAnimation FellaAnimationWidth = new();
+        public DoubleAnimation FellaAnimationHeight = new();
+        public MediaPlayer FellaSound = new();
+        public int imageWidth = 600;
+        public int imageHeight = 500;
+        public int animationTimerMillsec = 800;
+
+
+        public FellaWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private async void Init()
+        {
+            this.Topmost = true;
+
+            FellaSound.IsMuted = true;
+
+            await Task.Delay(750);
+
+            FellaSound.Open(new Uri("sound/facePunch.mp3", UriKind.Relative));
+            FellaSound.Stop();
+            FellaSound.IsMuted = false;
+
+            MainCanvas.Children.Add(FellaRect);
+            MainCanvas.Background = Brushes.Transparent;
+            FellaRect.Width = 0;
+            FellaRect.Height = 0;
+            FellaImage.BeginInit();
+            FellaImage.UriSource = new Uri("pack://application:,,,/png/fist.png");
+            FellaImage.EndInit();
+            FellaImageBrush.ImageSource = FellaImage;
+            FellaRect.Fill = FellaImageBrush;
+
+            // DOUBLE ANIMATIONS
+            FellaAnimationWidth.Duration = TimeSpan.FromMilliseconds(animationTimerMillsec);
+            //FellaAnimationWidth.AutoReverse = true;
+            FellaAnimationWidth.From = 0;
+            FellaAnimationWidth.To = imageWidth;
+
+            FellaAnimationHeight.Duration = TimeSpan.FromMilliseconds(animationTimerMillsec);
+            //FellaAnimationHeight.AutoReverse = true;
+            FellaAnimationHeight.From = 0;
+            FellaAnimationHeight.To = imageHeight;
+            // DOUBLE ANIMATION END
+
+        }
+
+        private void FellaRectPosSet()
+        {
+            this.Visibility = Visibility.Visible;
+
+            Canvas.SetTop(FellaRect, rN.Next(0, 800));
+            Canvas.SetLeft(FellaRect, rN.Next(0, 1000));
+        }
+
+        private async void ShowUpFella()
+        {
+        }
+    }
+}
--- MyLittleDesktopFella/MainWindow.xaml.cs
+++ MyLittleDesktopFella/MainWindow.xaml.cs
@@ -16,25 +16,10 @@ namespace MyLittleDesktopFella
 {
     public partial class MainWindow : Window
     {
-        private bool iniComplete = false;
-
-        private int imageWidth = 600;
-        private int imageHeight = 500;
-        private int animationTimerMillsec = 800;
-
-        private Random rN = new();
-
-        private Canvas MainCanvas = new();
-        private Rectangle FellaRect = new();
-        private BitmapImage FellaImage = new();
-        private ImageBrush FellaImageBrush = new();
-
         private DispatcherTimer MyLittleFellaRoutine = new();
 
-        private DoubleAnimation FellaAnimationWidth = new();
-        private DoubleAnimation FellaAnimationHeight = new();
 
-        public MediaPlayer FellaSound = new();
+        private bool iniComplete = false;
 
         public MainWindow()
         {
@@ -43,65 +28,30 @@ namespace MyLittleDesktopFella
             Initialize();
         }
 
-        public async void Initialize()
+        public void Initialize()
         {
-            FellaSound.IsMuted = true;
-
-            await Task.Delay(750);
-
-            FellaSound.Open(new Uri("sound/facePunch.mp3", UriKind.Relative));
-            FellaSound.Stop();
-            FellaSound.IsMuted = false;
-
-            MainGrid.Children.Add(MainCanvas);
-            MainCanvas.Children.Add(FellaRect);
-            MainCanvas.Background = Brushes.Transparent;
-            FellaRect.Width = 0;
-            FellaRect.Height = 0;
-            FellaImage.BeginInit();
-            FellaImage.UriSource = new Uri("pack://application:,,,/png/fist.png");
-            FellaImage.EndInit();
-            FellaImageBrush.ImageSource = FellaImage;
-            FellaRect.Fill = FellaImageBrush;
 
             // DISPATCHER-TIMER
             MyLittleFellaRoutine.Tick += MyLittleFellaRoutine_Tick;
             MyLittleFellaRoutineConfig();
             // DISPATCHER-TIMER END
 
-            // DOUBLE ANIMATIONS
-            FellaAnimationWidth.Duration = TimeSpan.FromMilliseconds(animationTimerMillsec);
-            //FellaAnimationWidth.AutoReverse = true;
-            FellaAnimationWidth.From = 0;
-            FellaAnimationWidth.To = imageWidth;
 
-            FellaAnimationHeight.Duration = TimeSpan.FromMilliseconds(animationTimerMillsec);
-            //FellaAnimationHeight.AutoReverse = true;
-            FellaAnimationHeight.From = 0;
-            FellaAnimationHeight.To = imageHeight;
-            // DOUBLE ANIMATION END
             iniComplete = true;
         }
 
         private void MyLittleFellaRoutineConfig()
         {
-            this.WindowState = WindowState.Minimized;
             FellaSound.Stop();
             FellaSound.Position = TimeSpan.Zero;
-            FellaRect.Visibility = Visibility.Hidden;
             MyLittleFellaRoutine.Stop();
-            MyLittleFellaRoutine.Interval = TimeSpan.FromMinutes(rN.Next(1, 60));
+            MyLittleFellaRoutine.Interval = TimeSpan.FromSeconds(rN.Next(5, 15));
             MyLittleFellaRoutine.Start();
         }
 
         private async void MyLittleFellaRoutine_Tick(object? sender, EventArgs e)
         {
-            //this.Topmost = true;
-            this.WindowState = WindowState.Maximized;
-
             FellaRectPosSet();
-
-            FellaRect.Visibility = Visibility.Visible;
 
             FellaRect.BeginAnimation(WidthProperty, FellaAnimationWidth);
             FellaRect.BeginAnimation(HeightProperty, FellaAnimationHeight);
@@ -115,10 +65,5 @@ namespace MyLittleDesktopFella
             MyLittleFellaRoutineConfig();
         }
 
-        private void FellaRectPosSet()
-        {
-            Canvas.SetTop(FellaRect, rN.Next(0, (int)MainCanvas.ActualHeight - imageHeight));
-            Canvas.SetLeft(FellaRect, rN.Next(0, (int)MainCanvas.ActualWidth - imageWidth));
-        }
     }
 }
