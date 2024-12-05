4ddf9652ff048aa3aa716ad1d2a8d81d45ee3a6f
Mullins182 <Mullins182@t-online.de>
2024-12-05 08:02:07 +0100

    testing aound ...

--- MyLittleDesktopFella/FellaWindow.xaml.cs
+++ MyLittleDesktopFella/FellaWindow.xaml.cs
@@ -12,12 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MyLittleDesktopFella
 {
     public partial class FellaWindow : Window
     {
 
+        private DispatcherTimer closeTimer;
         private Random rN = new();
         private Rectangle FellaRect = new();
         private BitmapImage FellaImage = new();
@@ -29,12 +31,25 @@ namespace MyLittleDesktopFella
         public static bool initFinished = false;
         private int animationTimerMillsec = 800;
 
-        public FellaWindow()
+
+    public FellaWindow()
         {
             InitializeComponent();
 
             Init();
         }
+
+        //public async void FellaWindow_FellaCall(object? sender, EventArgs e)
+        //{
+        //    while (!initFinished) { await Task.Delay(50); }
+
+        //    FellaRectPosSet();
+
+        //    FellaRect.BeginAnimation(WidthProperty, FellaAnimationWidth);
+        //    FellaRect.BeginAnimation(HeightProperty, FellaAnimationHeight);
+
+        //    this.Close();
+        //}
 
         public async void FellaWindow_FellaCall(object? sender, EventArgs e)
         {
@@ -45,7 +60,14 @@ namespace MyLittleDesktopFella
             FellaRect.BeginAnimation(WidthProperty, FellaAnimationWidth);
             FellaRect.BeginAnimation(HeightProperty, FellaAnimationHeight);
 
-            this.Close();
+            // Starten Sie einen Timer, um das Fenster nach 2 Sekunden zu schließen
+            closeTimer = new DispatcherTimer();
+            closeTimer.Interval = TimeSpan.FromSeconds(2);
+            closeTimer.Tick += (s, args) => {
+                this.Close();
+                closeTimer.Stop();
+            };
+            closeTimer.Start();
         }
 
         private void Init()
@@ -55,15 +77,32 @@ namespace MyLittleDesktopFella
             MainWindow.FellaCall += FellaWindow_FellaCall;
 
             // Canvas / Canvas Elements ini
+            MainCanvas.Width = 800;
+            MainCanvas.Height = 600;
             MainCanvas.Children.Add(FellaRect);
             MainCanvas.Background = Brushes.Transparent;
             FellaRect.Width = 0;
             FellaRect.Height = 0;
+
+
+            //FellaImage.BeginInit();
+            //FellaImage.UriSource = new Uri("pack://application:,,,/png/fist.png");
+            //FellaImage.DownloadCompleted += (s, e) => {
+            //    FellaImageBrush.ImageSource = FellaImage;
+            //    FellaRect.Fill = FellaImageBrush;
+            //};            
+            //FellaImage.EndInit();
+
             FellaImage.BeginInit();
-            FellaImage.UriSource = new Uri("pack://application:,,,/png/fist.png");
+            FellaImage.UriSource = new Uri("pack://application:,,,/MyLittleDesktopFella;component/png/fist.png", UriKind.Absolute);
+            FellaImage.DownloadCompleted += (s, e) => {
+                FellaImageBrush.ImageSource = FellaImage;
+                FellaRect.Fill = FellaImageBrush;
+                initFinished = true;
+            };
             FellaImage.EndInit();
-            FellaImageBrush.ImageSource = FellaImage;
-            FellaRect.Fill = FellaImageBrush;
+
+            //FellaRect.Fill = FellaImageBrush;
             // Canvas / Canvas Elements ini
 
             // DOUBLE ANIMATIONS
@@ -83,8 +122,15 @@ namespace MyLittleDesktopFella
 
         private void FellaRectPosSet()
         {
-            Canvas.SetLeft(FellaRect, rN.Next(0, (int)(MainCanvas.ActualWidth - FellaImage.Width)));
-            Canvas.SetTop(FellaRect, rN.Next(0, (int)(MainCanvas.ActualHeight - FellaImage.Height)));
+            int fWidth = (int)(MainCanvas.Width - FellaImage.Width);
+            int fHeight = (int)(MainCanvas.Height - FellaImage.Height);
+
+            //Canvas.SetLeft(FellaRect, rN.Next(0, fWidth));
+            //Canvas.SetTop(FellaRect, rN.Next(0, fHeight));
+
+            Canvas.SetLeft(FellaRect, 10);
+            Canvas.SetTop(FellaRect, 10);
+
         }
     }
 }
--- MyLittleDesktopFella/MainWindow.xaml.cs
+++ MyLittleDesktopFella/MainWindow.xaml.cs
@@ -61,7 +61,8 @@ namespace MyLittleDesktopFella
         private void MyLittleFellaRoutineConfig(int x, int y)
         {
             MyLittleFellaRoutine.Stop();
-            MyLittleFellaRoutine.Interval = TimeSpan.FromMinutes(rN.Next(x, y + 1));
+            //MyLittleFellaRoutine.Interval = TimeSpan.FromMinutes(rN.Next(x, y + 1));
+            MyLittleFellaRoutine.Interval = TimeSpan.FromSeconds(rN.Next(5, 7));
             MyLittleFellaRoutine.Start();
         }
 
